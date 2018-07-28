3eeb8d4dac12be1ee92b7d57cd683f8e4cedc633
Juan Martín González <gonzalezjuanmartin1@gmail.com>
2018-07-27 23:41:45 -0300

    Delete firebase.config.js

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: 'AIzaSyC9bHMfAr3GZBxH5mCgQX30TkYJsmr4iYA',
-	authDomain: 'webpacktodeploy.firebaseapp.com',
-	databaseURL: 'https://webpacktodeploy.firebaseio.com',
-	projectId: 'webpacktodeploy',
-	storageBucket: 'webpacktodeploy.appspot.com',
-	messagingSenderId: '304710438070'
-}
